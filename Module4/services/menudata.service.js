d31dd0b58d9fcabd40ddddd2ba81f811eece4427
Krish Jani <65438720+KrishJani@users.noreply.github.com>
2024-09-30 16:55:20 +0530

    Update menudata.service.js

--- Module4/services/menudata.service.js
+++ Module4/services/menudata.service.js
@@ -20,7 +20,7 @@
         service.getItemsForCategory = function(categoryShortName) {
             return $http({
                 method: "GET",
-                url: (ApiBasePath + '/menu_items/'+ categoryShortName + '.json')
+                url: (ApiBasePath + '/menu_items.json?category=' + categoryShortName)
             }).then(function(response) {
                 return response.data.menu_items;
             });
