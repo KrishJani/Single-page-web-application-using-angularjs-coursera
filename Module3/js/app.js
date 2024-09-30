04a683a97a0b09d91a45ad646923674c4756d93d
Krish Jani <65438720+KrishJani@users.noreply.github.com>
2024-09-30 15:49:42 +0530

    Update app.js

--- Module3/js/app.js
+++ Module3/js/app.js
@@ -4,7 +4,7 @@
     angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
         .service('MenuSearchService', MenuSearchService)
-        .constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json")
+        .constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com")
         .directive('foundItems', FoundItems);
 
     function FoundItems() {
